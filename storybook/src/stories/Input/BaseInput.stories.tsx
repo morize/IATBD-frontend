d0f4f6fcc759919527563dd31e427c2d6e647e8f
Mauriccio <mauricemr@outlook.com>
2021-03-17 00:10:14 +0100

    Initial setup app, storybook for components, base input component

---
+++ storybook/src/stories/Input/BaseInput.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import BaseInput, {IBaseInput} from './BaseInput';
+
+export default {
+    component: BaseInput,
+    title: 'Input/BaseInput',
+}
+
+export const BaseInputStory = (args: IBaseInput) => <BaseInput {...args}/>; 
+
+const args = {
+  placeholder: 'Placeholder text',
+  disabled: false,
+};
+
+BaseInputStory.args = args;
---
+++ storybook/src/stories/Input/BaseInput.tsx
@@ -0,0 +1,15 @@
+import React, { InputHTMLAttributes } from 'react';
+
+export interface IBaseInput extends InputHTMLAttributes<HTMLInputElement>{
+
+}
+
+const BaseInput = (args: IBaseInput) =>(
+    <div>
+        <label>hi</label>
+        <input {...args}/>
+    </div>   
+)
+
+
+export default BaseInput;
