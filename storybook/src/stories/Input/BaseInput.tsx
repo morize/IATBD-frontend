6399153cef1d0b569c20a65d4e1dd6b7e79f5096
Mauriccio <mauricemr@outlook.com>
2021-03-17 00:21:18 +0100

    setup prettier settings

--- storybook/src/stories/Input/BaseInput.tsx
+++ storybook/src/stories/Input/BaseInput.tsx
@@ -1,15 +1,12 @@
 import React, { InputHTMLAttributes } from 'react';
 
-export interface IBaseInput extends InputHTMLAttributes<HTMLInputElement>{
+export interface IBaseInput extends InputHTMLAttributes<HTMLInputElement> {}
 
-}
-
-const BaseInput = (args: IBaseInput) =>(
+const BaseInput = (args: IBaseInput) => (
     <div>
         <label>hi</label>
-        <input {...args}/>
-    </div>   
-)
-
+        <input {...args} />
+    </div>
+);
 
 export default BaseInput;
