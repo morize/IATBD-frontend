96385006acf1794d82904ff6a9987daad33b2e1d
Mauriccio <mauricemr@outlook.com>
2021-04-09 14:07:59 +0200

    added private route component

--- oppas-app/src/Routes/PrivateRoute.tsx
+++ oppas-app/src/Routes/PrivateRoute.tsx
@@ -1,10 +1,29 @@
-import { Redirect } from "react-router";
-import { Route } from "react-router-dom";
+import { Route, Navigate } from "react-router-dom";
 
 import { useCheckAuth } from "../Hooks/Auth";
 
-const PrivateRoute = ({ ...rest }) => {
-  return <Route />;
-};
+interface IPrivateRoute {
+  element: JSX.Element;
+  admin?: boolean;
+  path: string;
+}
 
+const PrivateRoute: React.FC<IPrivateRoute> = ({
+  element,
+  admin,
+  path,
+  children,
+}) => {
+  const condition = useCheckAuth();
+
+  if (condition === "loggedOut") {
+    return <Navigate to="../../account/inloggen" />;
+  }
+
+  return (
+    <Route path={path} element={element}>
+      {children}
+    </Route>
+  );
+};
 export default PrivateRoute;
--- oppas-app/src/Routes/Routes.tsx
+++ oppas-app/src/Routes/Routes.tsx
@@ -1,5 +1,6 @@
 import { Route, Routes } from "react-router-dom";
 
+import PrivateRoute from "../Routes/PrivateRoute";
 import Layout from "../Assets/Layout";
 import Home from "../Pages/Home";
 import Account from "../Pages/Account";
@@ -10,11 +11,13 @@ const PODRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Layout />}>
-        <Route path="/home" element={<Home />} />
-        <Route path="/account/" element={<Account />} />
-        <Route path="/account/inloggen" element={<Login />} />
-        <Route path="/account/aanmelden" element={<Register />} />
-        <Route path="/overzicht" />
+        <Route path="home" element={<Home />} />
+        <Route path="account/inloggen" element={<Login />} />
+        <Route path="account/aanmelden" element={<Register />} />
+
+        <PrivateRoute path="account" element={<Account />} />
+        <PrivateRoute path="overzicht/huisdieren" element={<Account />} />
+        <PrivateRoute path="overzicht/opassers" element={<Account />} />
       </Route>
     </Routes>
   );
