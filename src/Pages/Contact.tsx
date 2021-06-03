f85c8f6ea1c54d0cb145dc619714ade425644c17
morize <mauricemr@outlook.com>
2021-06-03 15:04:59 +0200

    definitive contact desktop version

--- src/Pages/Contact.tsx
+++ src/Pages/Contact.tsx
@@ -1,22 +1,26 @@
-import {
-  StH1,
-  StH2,
-  StSection,
-} from "../Utils/HTMLComponents";
+import { StH1, StH3, StP, StSection } from "../Utils/HTMLComponents";
+import BaseButton from "../Components/Button/BaseButton";
+import BaseInput from "../Components/Input/BaseInput";
+import Textarea from "../Components/Input/TextArea";
 
-const Contact = () => {
-  return (
-    <>
-      <StSection>
-        <StH1>Contact</StH1>
-        <StH2>Social Media</StH2>
-      </StSection>
+const Contact = () => (
+  <>
+    <StSection>
+      <StH1>Contact</StH1>
+      <StP>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. In cursus,
+        felis laoreet dignissim pharetra, nulla quam ullamcorper eros, ac
+        pellentesque metus odio quis massa.
+      </StP>
+    </StSection>
 
-      <StSection>
-        <StH2>Stuur ons een mail</StH2>
-      </StSection>
-    </>
-  );
-};
+    <StSection>
+      <StH3>Stuur ons een mail</StH3>
+      <BaseInput label="Uw naam:" />
+      <Textarea label="Uw bericht:" placeholder="Dit werkt nog niet" />
+      <BaseButton label="Stuur bericht" type="submit" />
+    </StSection>
+  </>
+);
 
 export default Contact;
